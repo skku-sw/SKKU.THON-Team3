7ee27af25c7b6dfce06cbbb01edec1fa08336806
Ddong-Min <bomur0522@gmail.com>
2023-09-23 21:57:31 +0900

    성공 실패 알림 수정

--- back/routes/board.js
+++ back/routes/board.js
@@ -8,7 +8,7 @@ module.exports = (db) => {
     db.query('SELECT * FROM board', (err, results) => {
         if(err){
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('게시글 조회 실패');
         }
         else{
             res.json(results);
@@ -21,7 +21,7 @@ module.exports = (db) => {
         db.query('SELECT * FROM board WHERE id = ?', [req.params.id], (err, results) => {
         if (err) {
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('게시글 조회 실패');
         } else {    
             db.query(`SELECT comment.nickname AS comment_nickname, comment.id, comment.content, comment.created_at
             FROM board
@@ -29,7 +29,7 @@ module.exports = (db) => {
             WHERE board.id = ?`, [req.params.id], (err, results2) => {
                 if (err) {
                     console.error('쿼리 실행 오류:', err);
-                    res.status(500).send('데이터베이스 오류');
+                    res.status(500).send('게시글 조회 실패');
                 } else {    
                     // 결과 처리
                     res.json({board: results, comment: results2});
@@ -41,30 +41,39 @@ module.exports = (db) => {
     
     //게시글 검색
     router.post('/search', (req, res) => {
-        const requestData = req.body.key;
-        console.log(requestData);
-        db.query('SELECT * FROM board WHERE title LIKE ?', ['%' + requestData + '%'], (err, results) => {
+        const requestData = req.body;
+        db.query('SELECT * FROM board WHERE title LIKE ?', ['%' + requestData.key + '%'], (err, results) => {
             if (err) {
-            console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
-            } else {    
-            // 결과 처리
-            res.json(results);
+                console.error('쿼리 실행 오류:', err);
+                res.status(500).send('데이터베이스 오류');
+            } else {
+                if (results.length === 0) {
+                    // 검색된 내용이 없는 경우
+                    res.status(404).send('검색 결과가 없습니다.');
+                } else {
+                    // 검색 결과가 있는 경우
+                    res.json(results);
+                }
             }
         });
     });
+    
 
     //게시글 작성
     router.post('/write', (req, res) => {
         const requestData = req.body;
+        if(requestData.title === '' || requestData.content === '' || requestData.nickname === ''){
+            res.status(500).send('게시글 작성 실패');
+        }
+        
         console.log(requestData);
         db.query('INSERT INTO board (title, content, nickname, created_at, like_count) VALUES (?, ?, ?, now(), 0)', [requestData.title, requestData.content, requestData.nickname], (err, results) => {
             if (err) {
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('게시글 작성 실패');
             } else {    
             // 결과 처리
-            res.json(results);
+            res.status(200).send('게시글 작성 성공');
             }
         });
     });
@@ -76,10 +85,10 @@ module.exports = (db) => {
         db.query('INSERT INTO comment (board_id, content, nickname, created_at) VALUES (?, ?, ?, now())', [requestData.board_id, requestData.content, requestData.nickname], (err, results) => {
             if (err) {
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('댓글 작성 실패');
             } else {    
             // 결과 처리
-            res.json(results);
+            res.status(200).send('댓글 작성 성공');
             }
         });
     });
@@ -89,10 +98,10 @@ module.exports = (db) => {
         db.query('UPDATE board SET like_count = like_count + 1 WHERE id = ?', [req.params.id], (err, results) => {
             if (err) {
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('좋아요 실패');
             } else {    
             // 결과 처리
-            res.json(results);
+            res.status(200).send('좋아요 성공');
             }
         });
     });
@@ -102,14 +111,31 @@ module.exports = (db) => {
         db.query('DELETE FROM board WHERE id = ?', [req.params.id], (err, results) => {
             if (err) {
             console.error('쿼리 실행 오류:', err);
-            res.status(500).send('데이터베이스 오류');
+            res.status(500).send('게시글 삭제 실패');
             } else {    
             // 결과 처리
-            res.json(results);
+            res.status(200).send('게시글 삭제 성공');
             }
         });
     });
 
+    //댓글 삭제
+    router.get('/comment/delete/:id', (req, res) => {
+        db.query('DELETE FROM comment WHERE id = ?', [req.params.id], (err, results) => {
+            if (err) {
+            console.error('쿼리 실행 오류:', err);
+            res.status(500).send('댓글 삭제 실패');
+            } else {    
+            // 결과 처리   
+                if(results.affectedRows === 0){
+                    res.status(404).send('댓글 삭제 실패')
+                }
+                else{
+                    res.status(200).send('댓글 삭제 성공');
+                }
+            }
+        });
+    });
 
   return router; // 이 부분이 함수 밖으로 나와야 합니다.
 };
