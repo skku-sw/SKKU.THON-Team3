87ac7ac7ddc0c438711760269f8a0482d3700d2c
yellu <wkwk0120@gmail.com>
2023-09-23 14:47:16 +0900

    Front Add

---
+++ front/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TestScreen from './screens/TestScreen';
+
+export default function App() {
+  return (
+    <TestScreen></TestScreen>
+  );
+}
+
+
---
+++ front/screens/TestScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+const TestScreen = () => {
+    return (
+        <Text>TEST</Text>
+    );
+};
+
+export default TestScreen;
